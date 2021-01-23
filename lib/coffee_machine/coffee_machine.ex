e7584486947e1f38ef6b7a7a9006a88b941eabbb
Massimo Biagioli <biagiolimassimo@gmail.com>
2021-01-23 22:23:13 +0100

    add receive_message function

--- lib/coffee_machine/coffee_machine.ex
+++ lib/coffee_machine/coffee_machine.ex
@@ -23,6 +23,24 @@ defmodule CoffeeMachine.CoffeeMachine do
     |> send_to_drink_maker()
   end
 
+  @doc """
+  Receive message from drink maker
+
+  ## Examples
+
+      iex> alias CoffeeMachine.CoffeeMachine
+      iex> CoffeeMachine.receive_message("M:Coffee with one sugar")
+      "Coffee with one sugar"
+
+  """
+  def receive_message(message) do
+    message
+    |> String.split(":")
+    |> process_message()
+  end
+
+  defp process_message(["M", message]), do: message
+
   defp format_for_drink_maker(%Order{} = order) do
     "#{serialize_order_type(order)}:#{serialize_quantity(order.sugar)}:#{serialize_quantity(order.sticks)}"
   end
